b98bd9b0b45efff93c45f0f5f0ff1aab9c72bc31
tsc529021953 <32382271+tsc529021953@users.noreply.github.com>
2023-09-08 21:30:04 +0800

    1.add image

--- app_xsyg_nft/src/main/java/com/sc/nft/MainActivity.java
+++ app_xsyg_nft/src/main/java/com/sc/nft/MainActivity.java
@@ -9,23 +9,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import timber.log.Timber;
 
+import static com.nbhope.lib_frame.utils.ViewUtil.immersionTitle;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        immersionTitle(this);
 
-        findViewById(R.id.text2).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent= new Intent();
-                intent.setAction("android.provider.Telephony.SECRET_CODE");
-                intent.setData( Uri.parse("android secret code://66"));
-                intent.setComponent(new ComponentName("com.sc.hetest","com.sc.hetest.receiver.StartReceiver"));
-                sendBroadcast(intent);
-//                Toast.makeText(this, "打开测试app", Toast.LENGTH_LONG).show();
-            }
-        });
     }
 }
